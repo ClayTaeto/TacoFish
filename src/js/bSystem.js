0a3d95f5425d5b5a59e1e6ea8781247b42c122b1
El Jay <ljninjaa@gmail.com>
2016-11-14 18:13:59 +0000

    tweaked battle system JS

--- src/js/bSystem.js
+++ src/js/bSystem.js
@@ -6,31 +6,35 @@ var bSystem = {
         sizes: ["bits", "tiny", "small", "medium", "medium-rare", "large", "xtra-large", "venti", "decaf-beast"],
         current: {},
     },
-    fish: function(location) {
-        var a = Math.random(), b, c, d = 1;
-        bSystem.getFish(((location) ? location : "LOCATION")); // Fills the fish arrays with fish from area that battle is in
-        if(a <= 0.00012207031) { // 1/8192 chance of golden caviar appearing with the fish
+    getFish(a) {
+        // Changes arrays in bSystem.fishes to match the fish in location (a)
+    },
+    fish: function(a) {
+        var b = Math.random(), c, d, f = 1;
+        bSystem.getFish(a); // Fills the fish arrays with fish from area that battle is in
+        if(b <= 0.00012207031) { // 1/8192 chance of golden caviar appearing with the fish
             this.caviar = true;
+            f *= 10;
         }else{
             this.caviar = false;
         }
-        if(a <= 0.85) { // 85% chance of a common fish
+        if(b <= 0.85) { // 85% chance of a common fish
             this.type = bSystem.fishes.types[Math.round(Math.random * ((bSystem.fishes.types).length - 1))];
-            d *= 1.1;
-        }else if(a <= 0.86) { // 1% chance of a super-rare fish
+        }else if(b <= 0.86) { // 1% chance of a super-rare fish
             this.type = bSystem.fishes.superRareTypes[Math.round(Math.random * ((bSystem.fishes.types).length - 1))];
-            d *= 5;
+            f *= 2.5;
         }else{ // 14% chance of a common-rare
             this.type = bSystem.fishes.rareTypes[Math.round(Math.random * ((bSystem.fishes.types).length - 1))];
-            d *= 1.5;
+            f *= 1.5;
         }
-        b = Math.round(Math.random() * ((bSystem.fishes.sizes).length - 1));
-        this.size = bSystem.fishes.sizes[b];
-        c = Math.max((b + 1) * 150, Math.round(Math.random() * ((b + 1) * 300))) * d;
-        this.hp = [c, c]; // [current HP, total HP]
+        c = Math.round(Math.random() * ((bSystem.fishes.sizes).length - 1));
+        this.size = bSystem.fishes.sizes[c];
+        c = Math.max((c + 1) * 150, Math.round(Math.random() * ((c + 1) * 300))) * f;
+        this.hp = [d, d]; // [current HP, total HP]
     },
     start: function() {
         var a = new bSystem.fish();
         bSystem.fishes.current = a;
+        alert("Caviar: " + a.caviar + ", Type: " + a.type + ", Size: " + a.size + ", HP: " + a.hp[0] + "/" + a.hp[1]);
     },
 };
